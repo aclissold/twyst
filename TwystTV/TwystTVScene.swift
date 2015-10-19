16befb551a5ccfa85bc57d66ab90424cadce6d4e
Andrew Clissold <andrewclissold@gmail.com>
2015-10-19 12:19:06 -0400

    Implement "twyst" up an octave

--- TwystTV/TwystTVScene.swift
+++ TwystTV/TwystTVScene.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2015 Andrew Clissold. All rights reserved.
 //
 
+import GameController
+
 class TwystTVScene: TwystScene, ArrowPressable, PlayPauseActivatable, UIGestureRecognizerDelegate {
 
     var playPauseActive = false
@@ -17,18 +19,53 @@ class TwystTVScene: TwystScene, ArrowPressable, PlayPauseActivatable, UIGestureR
     }
 
     func arrowPressed(direction: ArrowDirection) {
-        switch direction {
-        case .Up:
-            synthNode.frequency = playPauseActive ? Note.A4.rawValue : Note.D4.rawValue
-        case .Down:
-            synthNode.frequency = playPauseActive ? Note.C5.rawValue : Note.F4.rawValue
-        case .Left:
-            synthNode.frequency = playPauseActive ? Note.G4.rawValue : Note.C4.rawValue
-        case .Right:
-            synthNode.frequency = playPauseActive ? Note.B4.rawValue : Note.E4.rawValue
+        for controller in GCController.controllers() {
+            if let gravity = controller.motion?.gravity {
+                upAnOctave = gravity.z > -2.0/3.0
+                break
+            }
         }
 
+        synthNode.frequency = frequency(direction, playPauseActive, upAnOctave)
         synthNode.startPlaying()
+    }
+
+    /// Converts the Siri Remote's state into a note frequency.
+    func frequency(direction: ArrowDirection, _ upAFifth: Bool, _ upAnOctave: Bool) -> CGFloat {
+        switch (direction, upAFifth, upAnOctave) {
+        case (.Up, false, false):
+            return Note.D4.rawValue
+        case (.Up, true, false):
+            return Note.A4.rawValue
+        case (.Up, false, true):
+            return Note.D5.rawValue
+        case (.Up, true, true):
+            return Note.A5.rawValue
+        case (.Down, false, false):
+            return Note.F4.rawValue
+        case (.Down, true, false):
+            return Note.C5.rawValue
+        case (.Down, false, true):
+            return Note.F5.rawValue
+        case (.Down, true, true):
+            return Note.C6.rawValue
+        case (.Left, false, false):
+            return Note.C4.rawValue
+        case (.Left, true, false):
+            return Note.G4.rawValue
+        case (.Left, false, true):
+            return Note.C5.rawValue
+        case (.Left, true, true):
+            return Note.G5.rawValue
+        case (.Right, false, false):
+            return Note.E4.rawValue
+        case (.Right, true, false):
+            return Note.B4.rawValue
+        case (.Right, false, true):
+            return Note.E5.rawValue
+        case (.Right, true, true):
+            return Note.B5.rawValue
+        }
     }
 
 }
