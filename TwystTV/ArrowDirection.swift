caa9b7853ef11e7c467f676d7339d57eabac6f53
Andrew Clissold <andrewclissold@gmail.com>
2015-10-19 10:46:13 -0400

    Remove swipe gestures and refactor

---
+++ TwystTV/ArrowDirection.swift
@@ -0,0 +1,12 @@
+//
+//  ArrowDirection.swift
+//  Twyst
+//
+//  Created by Andrew Clissold on 10/19/15.
+//  Copyright © 2015 Andrew Clissold. All rights reserved.
+//
+
+/// A conceptual subset of `UIPressType` to allow stricter type checking.
+enum ArrowDirection {
+    case Up, Down, Left, Right
+}
--- TwystTV/TwystTVScene.swift
+++ TwystTV/TwystTVScene.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Andrew Clissold. All rights reserved.
 //
 
-class TwystTVScene: TwystScene, PlayPauseActivatable, UIGestureRecognizerDelegate {
+class TwystTVScene: TwystScene, ArrowPressable, PlayPauseActivatable, UIGestureRecognizerDelegate {
 
     var playPauseActive = false
 
@@ -14,58 +14,20 @@ class TwystTVScene: TwystScene, PlayPauseActivatable, UIGestureRecognizerDelegat
         super.didMoveToView(view)
 
         synthNode.synthType = .SineTink
-        addGestureRecognizers()
     }
 
-    func addGestureRecognizers() {
-        let upPressGestureRecognizer = UITapGestureRecognizer(target: self, action: "up")
-        let downPressGestureRecognizer = UITapGestureRecognizer(target: self, action: "down")
-        let leftPressGestureRecognizer = UITapGestureRecognizer(target: self, action: "left")
-        let rightPressGestureRecognizer = UITapGestureRecognizer(target: self, action: "right")
+    func arrowPressed(direction: ArrowDirection) {
+        switch direction {
+        case .Up:
+            synthNode.frequency = playPauseActive ? Note.A4.rawValue : Note.D4.rawValue
+        case .Down:
+            synthNode.frequency = playPauseActive ? Note.C5.rawValue : Note.F4.rawValue
+        case .Left:
+            synthNode.frequency = playPauseActive ? Note.G4.rawValue : Note.C4.rawValue
+        case .Right:
+            synthNode.frequency = playPauseActive ? Note.B4.rawValue : Note.E4.rawValue
+        }
 
-        let upSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "up")
-        let downSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "down")
-        let leftSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "left")
-        let rightSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "right")
-
-        upPressGestureRecognizer.allowedPressTypes = [UIPressType.UpArrow.rawValue]
-        downPressGestureRecognizer.allowedPressTypes = [UIPressType.DownArrow.rawValue]
-        leftPressGestureRecognizer.allowedPressTypes = [UIPressType.LeftArrow.rawValue]
-        rightPressGestureRecognizer.allowedPressTypes = [UIPressType.RightArrow.rawValue]
-
-        upSwipeGestureRecognizer.direction = [.Up]
-        downSwipeGestureRecognizer.direction = [.Down]
-        leftSwipeGestureRecognizer.direction = [.Left]
-        rightSwipeGestureRecognizer.direction = [.Right]
-
-        view?.addGestureRecognizer(upPressGestureRecognizer)
-        view?.addGestureRecognizer(downPressGestureRecognizer)
-        view?.addGestureRecognizer(leftPressGestureRecognizer)
-        view?.addGestureRecognizer(rightPressGestureRecognizer)
-
-        view?.addGestureRecognizer(upSwipeGestureRecognizer)
-        view?.addGestureRecognizer(downSwipeGestureRecognizer)
-        view?.addGestureRecognizer(leftSwipeGestureRecognizer)
-        view?.addGestureRecognizer(rightSwipeGestureRecognizer)
-    }
-
-    func up() {
-        synthNode.frequency = playPauseActive ? Note.A4.rawValue : Note.D4.rawValue
-        synthNode.startPlaying()
-    }
-
-    func down() {
-        synthNode.frequency = playPauseActive ? Note.C5.rawValue : Note.F4.rawValue
-        synthNode.startPlaying()
-    }
-
-    func left() {
-        synthNode.frequency = playPauseActive ? Note.G4.rawValue : Note.C4.rawValue
-        synthNode.startPlaying()
-    }
-
-    func right() {
-        synthNode.frequency = playPauseActive ? Note.B4.rawValue : Note.E4.rawValue
         synthNode.startPlaying()
     }
 
